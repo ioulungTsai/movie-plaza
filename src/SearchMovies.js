7165df543663d3e7d29cdee0847fa8c0f25a9d5c
brandonTsai <brandonya125@gmail.com>
2022-05-16 23:35:32 -0700

    create states of query and movies

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,28 +1,36 @@
-import React  from "react"
+import React, {useState}  from "react"
 
 export default function SearchMovie() {
+    const [query, setQuery] = useState('')
+    const [movies, setMovies] = useState([])
 
     const searchMovies = async (e) => {
         e.preventDefault()
-        console.log("submitting!")
-
-        const query = "Jurassic Park";
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
 
         try{
             const res = await fetch(url)
             const data = await res.json()
-            console.log(data)
+            setMovies(data.results);
         } catch(err) {
             console.error(err)
         }
     }
 
+    console.log(movies)
+
     return (
         <form className="form" onSubmit={searchMovies}>
             <label className="label" htmlFor="quert">Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="i.e. Jurassic Park"/>
+            <input
+                className="input"
+                type="text"
+                name="query"
+                placeholder="i.e. Jurassic Park"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
             <button className="button" type="submit" >Search</button>
         </form>
     )
